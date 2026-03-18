d5a414cd731c93929267db3998e002e874a9df0f
Sm-bello <bellosani2drescue@gmail.com>
2026-03-18 15:51:14 +0100

    Fix missing import and bracket error

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -456,7 +457,7 @@ class _DiscoverScreenState extends State<DiscoverScreen> {
           },
           child: CustomScrollView(
             physics: const AlwaysScrollableScrollPhysics(),
-          slivers: [
+            slivers: [
             SliverToBoxAdapter(
               child: Padding(
                 padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
